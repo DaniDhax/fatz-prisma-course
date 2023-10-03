f24dca6936c2bec622c8fbe6387b6aee6a599e9b
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-10-03 11:40:48 +0000

    index.js Hasta el delete

--- index.js
+++ index.js
@@ -3,14 +3,40 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient() 
 
 async function main() {
-    const newUser = await prisma.user.create ({
-        data: {
-            name: "Ryan",
-            email: "ryan@gmail.com"
+    // const newUser = await prisma.user.create ({
+    //     data: {
+    //         name: "Ryan",
+    //         email: "ryan@gmail.com"
+    //     }
+    // })
+    // console.log(newUser)
+
+
+
+    // const allUsers = await prisma.user.findMany()
+    // console.log(allUsers)
+    
+    
+    // const User = await prisma.user.findFirst({
+    //     where: {
+    //         OR: [
+    //             {id: 1},
+    //             {email: "maria@gmail.com"}
+    //         ]
+    //     }
+    // })
+    // console.log(User)
+
+
+    const user = await prisma.user.delete({
+        where: {
+            id: 3
         }
     })
-    console.log(newUser)
+    console.log(user)
+
 }
+
 
 main() //Tenemos que ejecutar la función main, porque si no 
         //no pasaría nada cuando ejecutamos 'node index.js', 
